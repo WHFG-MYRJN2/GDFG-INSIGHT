14521ed6663c9ede7a885b817efffb7ccab3c37c
WHFG-MYRJN2 <kemalrizqy71@gmail.com>
2026-08-11 13:48:29 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Ganti CACHE_VERSION setiap kali ada update file
 // ============================================================
 
-const CACHE_VERSION  = 'v1.0.7';
+const CACHE_VERSION  = 'v1.0.8';
 const CACHE_NAME     = 'monitoring-gdfg-' + CACHE_VERSION;
 
 // File yang di-cache saat install
